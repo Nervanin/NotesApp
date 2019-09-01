66e124f2bac22817cb28595d9b509eaa838af53f
Nervanin <Volua53@yandex.ru>
2019-09-01 11:07:18 +0300

    init

---
+++ NotesApp/NotesApp/NotesListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Konstantin on 01/09/2019.
+//  Copyright © 2019 Konstantin Meleshko. All rights reserved.
+//
+
+import UIKit
+
+class NotesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
